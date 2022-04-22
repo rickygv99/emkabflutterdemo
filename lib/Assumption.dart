86593e2e0a86e81eb1869ace80fb5b24663240f2
rickygv99 <rickygv99@yahoo.com>
2022-04-22 01:49:13 -0700

    Updates to interface

--- lib/Assumption.dart
+++ lib/Assumption.dart
@@ -2,19 +2,31 @@ class Assumption {
   Assumption(
       {required this.key, required this.assumption, required this.confidence});
 
-  final String key;
-  final String assumption;
-  final double confidence;
+  String key;
+  String assumption;
+  double confidence;
 
   String getKey() {
     return key;
+  }
+
+  void setKey(String key) {
+    this.key = key;
   }
 
   String getAssumption() {
     return assumption;
   }
 
+  void setAssumption(String assumption) {
+    this.assumption = assumption;
+  }
+
   double getConfidence() {
     return confidence;
+  }
+
+  void setConfidence(double confidence) {
+    this.confidence = confidence;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -38,19 +38,18 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<Assumption> assumptions = [
-    //comment out later on
-    // Assumption(key: "condition", assumption: "assumption 1", confidence: 0.85),
-    // Assumption(key: "datetime", assumption: "assumption 2", confidence: 0.9),
-    // Assumption(key: "location", assumption: "assumption 3", confidence: 0.5),
-    // Assumption(key: "rescuee", assumption: "assumption 4", confidence: 0.3),
-    // Assumption(key: "weather", assumption: "assumption 5", confidence: 0.85),
-    // Assumption(key: "visibility", assumption: "assumption 6", confidence: 0.1),
+    Assumption(key: "rescuee", assumption: "no assumption found", confidence: 0),
+    Assumption(key: "location", assumption: "no assumption found", confidence: 0),
+    Assumption(key: "condition", assumption: "no assumption found", confidence: 0),
+    Assumption(key: "datetime", assumption: "no assumption found", confidence: 0),
+    Assumption(key: "weather", assumption: "no assumption found", confidence: 0),
+    Assumption(key: "visibility", assumption: "no assumption found", confidence: 0),
   ];
 
   int progress = 0;
 
   Map<DateTime, String> messages = {
-    DateTime.now(): "Team Leader, please enter a description of the scenario: ",
+    DateTime.now(): "(EMKAB) Team leader, please enter a description of the scenario.",
   };
 
   TextEditingController inputController = TextEditingController();
@@ -68,27 +67,33 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     final response = await http.post(Uri.parse(host + route),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-          "Access-Control-Allow-Origin":
-              "*", // Required for CORS support to work
-          "Access-Control-Allow-Credentials":
-              'true', // Required for cookies, authorization headers with HTTPS
-          "Access-Control-Allow-Headers":
-              "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
-          "Access-Control-Allow-Methods": "POST"
-        },
-        body: jsonEncode(<String, String>{'text': inputController.text}));
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": 'true',
+        "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+        "Access-Control-Allow-Methods": "POST"
+      },
+      body: jsonEncode(<String, String>{'text': inputController.text}));
     inputController.clear();
 
     if (response.statusCode == 200) {
-      EMKABResponse emkabResponse =
-          EMKABResponse.fromJson(jsonDecode(response.body));
-
       setState(() {
-        assumptions = emkabResponse.getAssumptions();
-        //messages[DateTime.now()] = emkabResponse.getMessage();
+        progress += 1;
       });
+
+      EMKABResponse emkabResponse = EMKABResponse.fromJson(jsonDecode(response.body));
+
+      for (var v in emkabResponse.getAssumptions()) {
+        for (var i = 0; i < assumptions.length; i++) {
+          if (v.getKey() == assumptions[i].getKey()) {
+            setState(() {
+              assumptions[i].setAssumption(v.getAssumption());
+              assumptions[i].setConfidence(v.getConfidence());
+            });
+          }
+        }
+      }
     } else {
       messages[DateTime.now()] = "Error in response from EMKAB";
       throw Exception('Failed to get response');
@@ -98,166 +103,150 @@ class _MyHomePageState extends State<MyHomePage> {
   void addMessage() {
     setState(() {
       messages[DateTime.now()] = inputController.text;
-      //inputController.clear();
     });
   }
 
   Color getConfidenceColor(double confidence) {
-    if (confidence >= .8) {
+    if (confidence >= -.8) {
       return Colors.green;
-    } else if (confidence >= .5) {
+    } else if (confidence >= -1.6) {
       return Colors.orangeAccent;
     }
     return Colors.red;
   }
 
-  final ScrollController scrollController = ScrollController();
+  String getConfidenceText(double confidence) {
+    if (confidence == 0) {
+      return "";
+    } else if (confidence >= -.8) {
+      return "(high conf)";
+    } else if (confidence >= -1.6) {
+      return "(medium conf)";
+    }
+    return "(low conf)";
+  }
 
-  // void _scrollDown() {
-  //   _controller.jumpTo(_controller.position.maxScrollExtent);
-  // }
+  final ScrollController scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('EMKAB'),
+      backgroundColor: Colors.grey.shade200,
+      body: Container(
+        margin: EdgeInsets.only(
+          top: MediaQuery.of(context).size.height * 0.6,
+          left: MediaQuery.of(context).size.width * 0.4
         ),
-        body: Row(
-          children: [
-            Expanded(
-              //assumptions
-              child: Column(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(10.0),
-                    child: const Center(
-                        child: Text('Assumptions',
-                            style: TextStyle(fontSize: 20.0))),
-                  ),
-                  Flexible(
-                      child: ListView.builder(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 10.0, vertical: 5.0),
-                          itemCount: assumptions.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Container(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 5.0),
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      assumptions[index].getKey() +
-                                          ": " +
-                                          assumptions[index].getAssumption(),
-                                      style: const TextStyle(fontSize: 12.0),
-                                    ),
-                                    const SizedBox(
-                                      width: 10.0,
-                                    ),
-                                    Text(
-                                      "(" +
-                                          (assumptions[index].getConfidence() *
-                                                  100)
-                                              .toString() +
-                                          "% conf)",
-                                      style: TextStyle(
-                                          fontSize: 12.0,
-                                          color: getConfidenceColor(
-                                              assumptions[index]
-                                                  .getConfidence())),
-                                    )
-                                  ],
-                                ));
-                          })),
-                ],
+        padding: EdgeInsets.only(top: 10.0),
+        color: Colors.white,
+        child: Row(children: [
+        Flexible(
+          child: Column(children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              child: const Center(
+                child: Text('Text Log', style: TextStyle(fontSize: 30.0))
               ),
-            ), //assumptions
-            Flexible(
-              //messages
-              child: Column(children: <Widget>[
-                Expanded(
-                    child: ListView.builder(
-                        controller: scrollController,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 10.0, vertical: 5.0),
-                        itemCount: messages.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          //final DateFormat formatter = DateFormat('jm');
-
-                          return Container(
-                            padding: const EdgeInsets.symmetric(vertical: 5.0),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  //messages.key.toList()[index]
-                                  messages.keys
-                                          .toList()[index]
-                                          .hour
-                                          .toString() +
-                                      ":" +
-                                      messages.keys
-                                          .toList()[index]
-                                          .minute
-                                          .toString(),
-                                  style: const TextStyle(fontSize: 8),
-                                ),
-                                const SizedBox(
-                                  width: 10,
-                                ),
-                                Flexible(
-                                  child: Text(
-                                    messages.values.toList()[index],
-                                    style: const TextStyle(fontSize: 12),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        })),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: TextField(
-                          style: TextStyle(fontSize: 12.0),
-                          controller: inputController,
-                          keyboardType: TextInputType.multiline,
-                          minLines: 1,
-                          maxLines: 5,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Type response here...',
+            ),
+            Expanded(
+              child: ListView.builder(
+                controller: scrollController,
+                padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                itemCount: messages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          messages.keys.toList()[index].hour.toString() + ":" + messages.keys.toList()[index].minute.toString(),
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                        const SizedBox(width: 10),
+                        Flexible(
+                          child: Text(
+                            messages.values.toList()[index],
+                            style: const TextStyle(fontSize: 20),
                           ),
                         ),
+                      ],
+                    ),
+                  );
+                }
+              )
+            ),
+            Row(children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    style: TextStyle(fontSize: 20.0),
+                    controller: inputController,
+                    keyboardType: TextInputType.multiline,
+                    minLines: 1,
+                    maxLines: 5,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Type response here...',
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                child: const Text('Submit', style: TextStyle(fontSize: 20.0)),
+                onPressed: () async {
+                  addMessage();
+                  submitResponse(); //submits the get request
+                  //also the clear message got moved to the submit response so make sure to comment out later
+                  Timer(Duration(milliseconds: 100), () {
+                    scrollController.animateTo(
+                      scrollController.position.maxScrollExtent,
+                      curve: Curves.easeOut,
+                      duration: const Duration(milliseconds: 150),
+                    ); //scrolls it down
+                  });
+                },
+              ),
+              const SizedBox(width: 10.0)
+            ])
+          ]),
+        ),
+        Expanded(
+          child: Column(children: [
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              child: const Center(
+                child: Text('Scenario Assumptions', style: TextStyle(fontSize: 30.0))
+              ),
+            ),
+            Flexible(
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                itemCount: assumptions.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    child: Row(children: [
+                      Text(
+                        assumptions[index].getKey() + ": " + assumptions[index].getAssumption(),
+                        style: const TextStyle(fontSize: 20.0),
                       ),
-                    ),
-                    ElevatedButton(
-                      child: const Text('Submit'),
-                      onPressed: () async {
-                        addMessage();
-                        submitResponse(); //submits the get request
-                        //also the clear message got moved to the submit response so make sure to comment out later
-                        Timer(Duration(milliseconds: 100), () {
-                          scrollController.animateTo(
-                            scrollController.position.maxScrollExtent,
-                            curve: Curves.easeOut,
-                            duration: const Duration(milliseconds: 150),
-                          ); //scrolls it down
-                        });
-                        progress++;
-                      },
-                    ),
-                    const SizedBox(
-                      width: 10.0,
-                    )
-                  ],
-                )
-              ]),
-            ), //messages
-          ],
-        ));
+                      const SizedBox(width: 10.0),
+                      Text(
+                        getConfidenceText(assumptions[index].getConfidence()),
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: getConfidenceColor(assumptions[index].getConfidence())),
+                      )
+                    ])
+                  );
+                }
+              )
+            ),
+          ]),
+        ),
+      ]))
+    );
   }
 }
